c61f7f311658f293ba88db8292a8094975805b86
akildevs1000 <akildevs1000@gmail.com>
2025-05-05 11:55:10 +0400

    test

--- components/Order/Edit.vue
+++ components/Order/Edit.vue
@@ -596,7 +596,7 @@ export default {
       let rate = parseFloat(item?.price || 0);
       payload.quantity = qty;
       payload.rate = rate;
-      payload.tax = qty * rate;
+      payload.tax = 0;
       payload.total = qty * rate + payload.tax;
       this.getGrandTotal();
     },
@@ -604,14 +604,14 @@ export default {
       if (!item.item) return;
       let qty = parseFloat(item?.quantity || 0);
       let rate = parseFloat(item?.rate || 0);
-      item.tax = qty * rate;
+      item.tax = 0;
       item.total = qty * rate + item.tax;
       this.getGrandTotal();
     },
     doTaxCalculate(item) {
       let qty = parseFloat(item?.quantity || 0);
       let rate = parseFloat(item?.rate || 0);
-      let tax = parseFloat(item?.tax || 0);
+      let tax = parseFloat(0);
       item.total = qty * rate + tax;
       this.getGrandTotal();
     },
